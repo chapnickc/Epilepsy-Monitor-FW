fdd7d66648d6e075a4360d2f3ec78b8ca6a55481
chapnickc <chapnickc@slu.edu>
2016-08-23 15:05:53 -0500

    increased sampling frequency and values now print on one line

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -10,22 +10,25 @@
 #define I2C_SDA     p4
 #define I2C_SCL     p5
 
-
-uint8_t tempSensorAddress = 0x80;              // 7-bit address of TMP006 can be 0x40-0x47,  converted to 8-bit address here
 uint8_t max30100_addr = 0xAE;
 
+uint8_t tmp006_addr = 0x80;              // 7-bit address of TMP006 can be 0x40-0x47,  converted to 8-bit address here
 uint16_t tempSamples = TMP006_CFG_2SAMPLE;
+
+
+
+
 
 Serial pc(USBTX, USBRX);
 DigitalOut led1(LED1);
 
 
-MAX30100 sensor(I2C_SDA, I2C_SCL, max30100_addr);
-TMP006 healthThermometer(I2C_SDA, I2C_SCL, tempSensorAddress); 
+MAX30100 hrSensor(I2C_SDA, I2C_SCL, max30100_addr);
+TMP006 tempSensor(I2C_SDA, I2C_SCL, tmp006_addr); 
 
 
 float temperature;
-float T = 1.00;
+float T = 0.050;
 
 // HR Calculation variables
 //float T = 0.050;
@@ -44,29 +47,30 @@ std::vector<float> IR_data(samples);
 
 /*          main            */
 int main(void) {
+
     pc.baud(115200);
-    //sensor.init(pw1600, sr50, high, i44, i44);
-    sensor.begin(pw1600, i44, sr100);               // pw1600 allows for 16-bit resolution
-    healthThermometer.config(tempSensorAddress, tempSamples);
+    //hrSensor.init(pw1600, sr50, high, i44, i44);
+    hrSensor.begin(pw1600, i44, sr100);               // pw1600 allows for 16-bit resolution
+    tempSensor.config(tmp006_addr, tempSamples);
 
     while (true) {
         led1 =1;
 
-  //      sensor.readSensor();
-        temperature = healthThermometer.readObjTempC(tempSensorAddress);
+        hrSensor.readSensor();
+        temperature = tempSensor.readObjTempC(tmp006_addr);
 
         // unsigned int is guaranteed to be at least 16 bits
-        //pc.printf("IR: %u\r\n", (unsigned int) sensor.IR);  
+        pc.printf("IR: %u | Temp (deg C): %f\r\n", (unsigned int) hrSensor.IR, temperature);  
         
-        pc.printf("Temp: %f\r\n",  temperature);
+        //pc.printf("Temp: %f\r\n",  temperature);
 
         wait(T);
 
         /*
         uint8_t i;
         for(i=0; i < samples; i++){
-            sensor.readSensor();
-            IR_data[i] = (float) sensor.IR;
+            hrSensor.readSensor();
+            IR_data[i] = (float) hrSensor.IR;
             wait(T);                            // will take 5.00 seconds
         }
         
