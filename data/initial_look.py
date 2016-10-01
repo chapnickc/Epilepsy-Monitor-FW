fe030fa100aba63be7e8fb536675f55fff1f6c67
chapnickc <chapnickc@slu.edu>
2016-09-30 20:52:02 -0500

    -

--- data/initial_look.py
+++ data/initial_look.py
@@ -1,16 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from filters import *
+
 with open('./recording_20160910_223912.txt') as f:
     lines = f.readlines()
 
-rec1 = np.array([])
+x = np.array([])
 for line in lines:
-    rec1 = np.append(rec1, float(line.strip()))
+    x = np.append(x, float(line.strip()))
 
 
-plt.plot(rec1)
+T = 0.050
+fs = 1/T
+lowcut = 0.40
+highcut = 4.00
+order = 5
+
+x_bp = butter_bandpass_filter(x, lowcut, highcut, fs, order)
+
+plt.plot(x)
 plt.show()
+
+plt.plot(x_bp)
+plt.show()
+
+
 
 
 
--- src/simulate/main.cpp
+++ src/simulate/main.cpp
@@ -1,4 +1,3 @@
-
 #include <vector>
 
 #include "mbed.h"
